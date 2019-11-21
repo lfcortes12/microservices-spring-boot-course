dbda15564ab201b309d21fedb76904519bad4a46
Luis Cortes <lfcortes12@gmail.com>
2019-11-21 08:35:00 -0500

    adds application base

---
+++ rest-full-webservices/src/main/java/com/glb/training/restfullwebservices/RestFullWebservicesApplication.java
@@ -0,0 +1,13 @@
+package com.glb.training.restfullwebservices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestFullWebservicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestFullWebservicesApplication.class, args);
+	}
+
+}
