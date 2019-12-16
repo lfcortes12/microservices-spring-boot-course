b22cac39f57eba458445e401d826c6e38cdae852
Luis Cortes <lfcortes12@gmail.com>
2019-12-16 05:43:10 -0500

    add content negotiation with xml

--- rest-full-webservices/src/test/java/com/glb/training/restfullwebservices/RestFullWebservicesApplicationTests.java
+++ rest-full-webservices/src/test/java/com/glb/training/restfullwebservices/RestFullWebservicesApplicationTests.java
@@ -3,11 +3,17 @@ package com.glb.training.restfullwebservices;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.dataformat.xml.XmlMapper;
+import com.glb.training.restfullwebservices.model.Estimation;
+
 @SpringBootTest
 class RestFullWebservicesApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads() throws JsonProcessingException {
+		XmlMapper mapper = new XmlMapper();
+		System.out.println(mapper.writeValueAsString(new Estimation()));
 	}
 
 }
